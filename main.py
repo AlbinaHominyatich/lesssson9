a651dc3b9e6dd710a56760761011ee8c4128e3a9
AlbinaHominyatich <hominyatichalbina@gmail.com>
2022-11-19 16:17:01 +0200

    дістали усі доллари

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import requests
 response = requests.get('https://uk.wikipedia.org/')
 print(type(response.text))
+#БІТОК
+
+response = requests.get('https://coinmarketcap.com/')
+response_text = response.text
+response_parse = response_text.split('<span>')
+for parse_elem_1 in response_parse:
+    if parse_elem_1.startswith("$"):
+        print(parse_elem_1)
+
